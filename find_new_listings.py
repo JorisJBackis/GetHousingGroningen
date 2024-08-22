01234bce95a0667d25002d4d8515e10218f5cd29
JorisJBackis <joris.backis@student.nhlstenden.com>
2024-08-22 19:22:01 +0300

    actually commited the timer changes

--- find_new_listings.py
+++ find_new_listings.py
@@ -24,7 +24,7 @@ def main():
         # Get the current time as a string in "HH:MM" format
         current_time_str = time.strftime("%H:%M")
         start_time_str = "08:00"
-        end_time_str = "17:30"
+        end_time_str = "19:30"
 
         # Check if the current time is within working hours
         if start_time_str <= current_time_str <= end_time_str:
@@ -33,14 +33,14 @@ def main():
             new_listings = detect_new_listings(current_listings, previous_listings)
 
             if new_listings:
-                send_email(new_listings, 'jorisbackis@gmail.com')  # Replace with your email
+                send_email(new_listings, 'jorisbackis@gmail.com')  # Replace with email where you wish to receive notifications
                 save_current_listings(current_listings)
             else:
                 print("No new listings found.")
 
             # Wait for a random interval (8 to 22 minutes) in order to simulate real human activity
             wait_time = random.randint(8*60, 22*60)  # Convert minutes to seconds
-            print(f"Waiting for {wait_time} seconds before the next run.")
+            print(f"Waiting for {wait_time // 60} minutes {wait_time % 60} seconds before the next run.")
             time.sleep(wait_time)
         else:
             print("Outside of working hours. Exiting.")
